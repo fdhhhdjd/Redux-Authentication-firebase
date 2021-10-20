80769fab5e7eedf57ba030bbdb8631f7d1f7655a
tai <nguyentientai10@gmail.com>
2021-10-20 22:57:24 +0700

    import

---
+++ .history/src/import/index_20211020225424.js
@@ -0,0 +1,3 @@
+export { default as Forget } from "../Components/Forget";
+export { default as Login } from "../Page/Login";
+export { default as Register } from "../Page/Register";
---
+++ src/import/index.js
@@ -0,0 +1,4 @@
+export { default as Forget } from "../Page/Forget";
+export { default as Login } from "../Page/Login";
+export { default as Register } from "../Page/Register";
+export { default as Contents } from "../Components/Contents";
